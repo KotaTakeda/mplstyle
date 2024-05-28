caf93a3dbd82a67352ba51517b2cf4dcd0be447f
KotaTakeda <takeda11.kl@outlook.jp>
2024-05-28 20:40:48 +0900

    remove comment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import matplotlib.pyplot as plt
 from matplotlib_inline.backend_inline import set_matplotlib_formats
 
-# from states import to_mat_rep
-
 set_matplotlib_formats("pdf", "svg")
 plt.style.use("vis.mplstyle")
